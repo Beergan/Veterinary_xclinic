9f7db34936d2d2eaa02c99b6f4592179e971d1bb
Beergan <138391295+Beergan@users.noreply.github.com>
2025-10-28 10:36:38 +0700

    update

--- src/SLK.XClinic.ModuleVeternay/obj/Debug/net8.0/SLK.XClinic.ModuleVeternay.AssemblyInfo.cs
+++ src/SLK.XClinic.ModuleVeternay/obj/Debug/net8.0/SLK.XClinic.ModuleVeternay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SLK.XClinic.ModuleVeternay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a26815d200b97db8ef452ef22d9d91c2f63d8720")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bc7173d9befffee17e6217ba8498105724461e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SLK.XClinic.ModuleVeternay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SLK.XClinic.ModuleVeternay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
