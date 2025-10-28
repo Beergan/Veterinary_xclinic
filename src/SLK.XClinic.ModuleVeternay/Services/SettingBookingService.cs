2e4d76c3413abf0a3dfe10771adeefecd577c387
Beergan <138391295+Beergan@users.noreply.github.com>
2025-10-28 09:51:38 +0700

    update

--- src/SLK.XClinic.ModuleVeternay/Services/SettingBookingService.cs
+++ src/SLK.XClinic.ModuleVeternay/Services/SettingBookingService.cs
@@ -88,6 +88,7 @@ public class SettingBookingService : MyServiceBase, ISettingBookingService
             }
             else
             {
+               
                 info.GuidEmployee = _ctx.GuidEmployee;
                 if (info.BookingServices != null)
                 {
